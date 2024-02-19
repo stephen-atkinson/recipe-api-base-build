84482b98ef33ded8ba82a5107a4677142e96b45a
stephenatkinson <stephen.p.atkinson@outlook.com>
2024-02-19 09:52:53 +0000

    Added missing ApiVersionNeutral attribute.

--- Recipes.Api/Controllers/WeatherForecastController.cs
+++ Recipes.Api/Controllers/WeatherForecastController.cs
@@ -5,6 +5,7 @@ namespace Recipes.Api.Controllers;
 
 [ApiController]
 [Route("[controller]")]
+[ApiVersionNeutral]
 public class WeatherForecastController : ControllerBase
 {
     private static readonly string[] Summaries = {
