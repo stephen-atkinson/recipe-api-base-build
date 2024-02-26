41b2d9e04c4db89d507ad7df58e69598ec410af0
stephenatkinson <stephen.p.atkinson@outlook.com>
2024-02-26 14:13:37 +0000

    Recipe.Course is now nullable.

--- Recipes.Core/Domain/Recipe.cs
+++ Recipes.Core/Domain/Recipe.cs
@@ -6,7 +6,7 @@ public class Recipe
     public string Name { get; set; } = null!;
     public string Instructions { get; set; } = null!;
     public Diet? Diet { get; set; }
-    public Course Course { get; set; }
+    public Course? Course { get; set; }
     public DateTime Created { get; set; }
     public DateTime LastUpdated { get; set; }
     public ICollection<Ingredient> Ingredients { get; set; } = null!;
